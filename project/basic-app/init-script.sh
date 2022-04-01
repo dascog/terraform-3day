f5aa8d0c492f81efed86013ea0e30e8524f242f3
Darryl Greig <darryl.greig@owerya.com>
2022-04-01 16:53:05 +0100

    removed a .jar that was mucking up the deployment

--- project/basic-app/init-script.sh
+++ project/basic-app/init-script.sh
@@ -4,4 +4,4 @@ yum -y install java-1.8.0
 cd /home/ec2-user
 # wget http://training.conygre.com/compactdiscapp.jar
 wget https://tinyurl.com/CompactDiscRestNoDatabase
-nohup java -jar CompactDiscRestNoDatabase.jar > ec2dep.log
+nohup java -jar CompactDiscRestNoDatabase > ec2dep.log
