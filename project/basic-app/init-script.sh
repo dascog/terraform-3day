16581f177f06e0ae8aa978f4f80675794063d1c6
Darryl Greig <darryl.greig@owerya.com>
2022-04-08 21:44:56 +0100

    change to script for basic app

--- project/basic-app/init-script.sh
+++ project/basic-app/init-script.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 yum update -y
 yum -y install java-1.8.0
+yum install ec2-instance-connect
 cd /home/ec2-user
 # wget http://training.conygre.com/compactdiscapp.jar
 wget https://tinyurl.com/CompactDiscRestNoDatabase
